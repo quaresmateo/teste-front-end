26e1748f59cd566a4b690263a268b4b7ae4b3e60
quaresmateo <teoian.giovane@gmail.com>
2020-12-31 14:31:50 -0300

    Estiliza página de busca

--- src/app/views/search-repo/search-repo.component.ts
+++ src/app/views/search-repo/search-repo.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-repo.component.scss']
 })
 export class SearchRepoComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
