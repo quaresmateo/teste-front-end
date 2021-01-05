b626807fde8dfd1863b6ac23e8061d4ad4d7042c
quaresmateo <teoian.giovane@gmail.com>
2021-01-05 10:22:32 -0300

    Valida formulário

--- src/app/components/searches/searches.component.ts
+++ src/app/components/searches/searches.component.ts
@@ -3,6 +3,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchesService } from './searches.service';
 import { Users } from './user/users.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-searches',
@@ -12,10 +13,10 @@ import { Users } from './user/users.model';
 export class SearchesComponent implements OnInit {
   constructor(private services: SearchesService) {}
   users: Users;
-  text: string;
+  text = '';
   sort: string;
-  gridOptions;
   rowData;
+  validatingForm: FormGroup;
 
   columnDefs = [
     { field: 'id' },
@@ -24,18 +25,31 @@ export class SearchesComponent implements OnInit {
   ];
 
   ngOnInit(): void {
-    if (this.text !== undefined) {
+    this.validatingForm = new FormGroup({
+      minLength: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1)
+      ])
+    });
+
+    if (this.text.length > 0) {
       this.find('');
     }
   }
 
   find(value: string) {
     this.text = value;
-    this.services
-      .getUsers({ query: this.text, sort: this.sort })
-      .subscribe((users: Users) => {
-        this.users = users;
-        this.rowData = this.users.items;
-      });
+    if (this.text.length > 0) {
+      this.services
+        .getUsers({ query: this.text, sort: this.sort })
+        .subscribe((users: Users) => {
+          this.users = users;
+          this.rowData = this.users.items;
+        });
+    }
+  }
+
+  get input() {
+    return this.validatingForm.get('minLength');
   }
 }
