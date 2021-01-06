f1d0b45292b13de8c3f23ce61904a6023adfe4e6
quaresmateo <teoian.giovane@gmail.com>
2021-01-06 12:09:30 -0300

    Cria componente de usuários mais ativos

---
+++ src/app/components/active-users/active-users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-active-users',
+  templateUrl: './active-users.component.html',
+  styleUrls: ['./active-users.component.scss']
+})
+export class ActiveUsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
