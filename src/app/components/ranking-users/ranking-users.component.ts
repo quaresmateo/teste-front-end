280691bd28457c9e988dd3085d529f96ac88f114
quaresmateo <teoian.giovane@gmail.com>
2021-01-06 15:01:47 -0300

    Cria componente de usuários mais ativos

---
+++ src/app/components/ranking-users/ranking-users.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Users } from '../searches/user/users.model';
+import { RankingUsersService } from './ranking-users.service';
+
+@Component({
+  selector: 'app-ranking-users',
+  templateUrl: './ranking-users.component.html',
+  styleUrls: ['./ranking-users.component.scss']
+})
+export class RankingUsersComponent implements OnInit {
+  users: Users;
+  query = 'location:brazil+location:brazil';
+  constructor(private services: RankingUsersService) {}
+
+  ngOnInit(): void {
+    this.services.getUsers({ query: this.query }).subscribe((users: Users) => {
+      this.users = users;
+      console.log(users);
+      // this.rowData = this.users.items;
+    });
+  }
+}
